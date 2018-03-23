2b0ff0ead766d56c58440df5e4c0571ed160cb88
Javier Gamarra <nhpatt@gmail.com>
2018-03-23 13:10:55 +0100

    new bot

--- Bot/messages/index.js
+++ Bot/messages/index.js
@@ -1,72 +1,64 @@
-/*-----------------------------------------------------------------------------
-This template demonstrates how to use an IntentDialog with a LuisRecognizer to add 
-natural language support to a bot. 
-For a complete walkthrough of creating this type of bot see the article at
-https://aka.ms/abs-node-luis
------------------------------------------------------------------------------*/
-"use strict";
-var builder = require("botbuilder");
-var botbuilder_azure = require("botbuilder-azure");
-var path = require('path');
+'use strict';
 
-var useEmulator = (process.env.NODE_ENV == 'development');
+const builder = require('botbuilder');
+const botbuilder_azure = require('botbuilder-azure');
+const path = require('path');
 
-var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
+let useEmulator = (process.env.NODE_ENV === 'development');
+useEmulator = true;
+
+const connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+	appId: process.env['MicrosoftAppId'],
+	appPassword: process.env['MicrosoftAppPassword'],
+	openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
-
-var tableName = 'botdata';
-var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
-
-var bot = new builder.UniversalBot(connector);
+const bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
-bot.set('storage', tableStorage);
 
 // Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+const luisAppId = process.env.LuisAppId;
+const luisAPIKey = process.env.LuisAPIKey;
+const luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
 
 const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
 
 // Main dialog with LUIS
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-.matches('Greeting', (session) => {
-    session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
-})
-.matches('Help', (session) => {
-    session.send('You reached Help intent, you said \'%s\'.', session.message.text);
-})
-.matches('Cancel', (session) => {
-    session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
-})
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
-});
+const recognizer = new builder.LuisRecognizer(LuisModelUrl);
+const intents = new builder.IntentDialog({recognizers: [recognizer]})
+	.matches('Greeting', (session) => {
+		session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+	})
+	.matches('Help', (session) => {
+		session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+	})
+	.matches('Cancel', (session) => {
+		session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
+	})
+	/*
+	.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+	*/
+	.onDefault((session) => {
+		session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+	});
 
-bot.dialog('/', intents);    
+
+// https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9cfb77bc-f570-4d4e-a138-21748defb3af?subscription-key=f1497b363d484310830cf4fb46e71472&verbose=false&q=hi
+
+// let bot = new builder.UniversalBot(connector, function (session) {
+//     session.send("You said: %s", session.message.text);
+// });
+
+bot.dialog('/', intents);
 
 if (useEmulator) {
-    var restify = require('restify');
-    var server = restify.createServer();
-    server.listen(3978, function() {
-        console.log('test bot endpont at http://localhost:3978/api/messages');
-    });
-    server.post('/api/messages', connector.listen());    
+	const restify = require('restify');
+	const server = restify.createServer();
+	server.listen(3978, function () {
+		console.log('test bot endpoint at http://localhost:3978/api/messages');
+	});
+	server.post('/api/messages', connector.listen());
 } else {
-    module.exports = connector.listen();
+	module.exports = connector.listen();
 }
 
